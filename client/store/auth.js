9eee5806422f14b175fc3f32ce101853980cc37e
Maciej Krzemianowski <git@easiestsolution.eu>
2020-04-29 23:22:36 +0200

    Auth / login request & store setup WiP

---
+++ client/store/auth.js
@@ -0,0 +1,78 @@
+import {
+  LOGIN_SUCCESS,
+  LOGOUT,
+  REQUEST_ERROR,
+  REQUEST_PENDING,
+  REQUEST_SUCCESS,
+} from './action-types';
+import restApiRequest from '@/restApiRequest';
+
+const initialState = {
+  loggedIn: !!localStorage.getItem('token'),
+  userEmail: null,
+};
+
+const mutations = {
+  [LOGIN_SUCCESS](state, { email }) {
+    state.loggedIn = true;
+    state.userEmail = email;
+  },
+  [LOGOUT](state) {
+    state.userData = initialState.userData;
+    state.loggedIn = initialState.loggedIn;
+  },
+};
+
+const actions = {
+  login({ commit, state }, authData) {
+    if (state.login) {
+      return;
+    }
+
+    commit(REQUEST_PENDING, null, {
+      root: true,
+    });
+
+    restApiRequest({
+      collection: 'auth',
+      actionType: 'create',
+      data: authData,
+    })
+      .then(token => {
+        commit(REQUEST_SUCCESS, null, {
+          root: true,
+        });
+
+        commit(LOGIN_SUCCESS, {
+          token,
+          email: authData.email,
+        });
+
+        localStorage.setItem('token', token);
+        this.app.router.push('/');
+      })
+      .catch(err => {
+        // eslint-disable-next-line
+        console.error(err);
+
+        commit(REQUEST_ERROR, err, {
+          root: true,
+        });
+      });
+  },
+  logout({ commit }) {
+    localStorage.removeItem('token');
+
+    commit({
+      type: LOGOUT,
+    });
+    this.app.router.push('login');
+  },
+};
+
+export default {
+  namespaced: true,
+  state: initialState,
+  mutations,
+  actions,
+};
---
+++ client/store/global.js
@@ -0,0 +1,39 @@
+import {
+  REQUEST_ERROR,
+  REQUEST_PENDING,
+  REQUEST_SUCCESS,
+  CLEAR_ERRORS,
+} from './actionTypes';
+
+const initialState = {
+  loading: false,
+  errors: null,
+};
+
+const mutations = {
+  [REQUEST_SUCCESS](state) {
+    state = { ...state, initialState };
+  },
+  [REQUEST_PENDING](state) {
+    state.loading = true;
+  },
+  [REQUEST_ERROR](state, error) {
+    state.loading = false;
+    state.errors = state.errors ? [...state.errors, error] : [error];
+  },
+  [CLEAR_ERRORS](state) {
+    state.errors = null;
+  },
+};
+
+const actions = {
+  clearErrors({ commit }) {
+    commit(CLEAR_ERRORS);
+  },
+};
+
+export default {
+  state: initialState,
+  mutations,
+  actions,
+};
