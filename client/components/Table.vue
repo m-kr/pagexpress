46339385e8ef844df0586f55fca2ed8e078af3ed
Maciek Krzemianowski <maciej.krzemianowski@itech.media>
2020-05-02 00:43:36 +0200

    Create placeholder for future Table component.

---
+++ client/components/Table.vue
@@ -0,0 +1,47 @@
+<template>
+  <table class="table is-fullwidth is-striped">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>URL</th>
+        <th>Description</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Awesome page name</td>
+        <td>/some-url/</td>
+        <td>Another awesome page</td>
+        <td>
+          <div class="buttons are-small">
+            <button class="button is-info is-light">Edit</button>
+            <button class="button is-danger is-light">Delete</button>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>Awesome page name</td>
+        <td>/some-url/</td>
+        <td>Another awesome page</td>
+        <td>
+          <div class="buttons are-small">
+            <button class="button is-info is-light">Edit</button>
+            <button class="button is-danger is-light">Delete</button>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>Awesome page name</td>
+        <td>/some-url/</td>
+        <td>Another awesome page</td>
+        <td>
+          <div class="buttons are-small">
+            <button class="button is-info is-light">Edit</button>
+            <button class="button is-danger is-light">Delete</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
