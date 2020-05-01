96288ac0b16e8386c3a3684dd67bebf0981de611
Maciek Krzemianowski <maciej.krzemianowski@itech.media>
2020-05-02 00:46:08 +0200

    Use Table placeholder in the main page.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,18 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Nuxt Auth</h1>
+      <h1 class="title">Pages list</h1>
+      <Table />
     </div>
   </section>
 </template>
+
+<script>
+import Table from '~/components/Table';
+
+export default {
+  components: {
+    Table,
+  },
+};
+</script>
