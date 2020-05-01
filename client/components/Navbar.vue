28c1d0735e79cdccc1b7e22a0036e3e908b33efe
Maciek Krzemianowski <maciej.krzemianowski@itech.media>
2020-05-02 00:43:07 +0200

    Improve Navbar layout.

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,20 +1,19 @@
 <template>
-  <nav class="navbar is-light">
+  <nav class="navbar">
     <div class="container">
       <div class="navbar-brand">
         <nuxt-link class="navbar-item" to="/">Pagexpress</nuxt-link>
-        <button class="button navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
       </div>
-      <div class="navbar-menu">
-        <div v-if="isAuthenticated" class="navbar-end">
-          <a class="navbar-item" @click.prevent="logout">Logout</a>
-        </div>
-        <div v-else class="navbar-end">
-          <nuxt-link class="navbar-item" to="/sign-in">Sign In</nuxt-link>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <div v-if="isAuthenticated" class="navbar-item">
+            <a class="button is-light" @click.prevent="logout">Logout</a>
+          </div>
+          <div v-else class="navbar-item">
+            <nuxt-link class="button is-primary navbar-item" to="/sign-in"
+              ><strong>Sign In</strong></nuxt-link
+            >
+          </div>
         </div>
       </div>
     </div>
