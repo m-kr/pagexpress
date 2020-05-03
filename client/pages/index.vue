769ec73d2fa29114caed6908be97ccd29c317c11
Maciek Krzemianowski <maciej.krzemianowski@itech.media>
2020-05-03 23:15:11 +0200

    Minor semantic fixes

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -13,7 +13,7 @@
           <div class="level-item">
             <div class="field has-addons">
               <p class="control">
-                <input class="input" type="text" placeholder="Find a post" />
+                <input class="input" type="text" placeholder="Find title" />
               </p>
               <p class="control">
                 <button class="button">
@@ -26,10 +26,6 @@
 
         <!-- Right side -->
         <div class="level-right">
-          <p class="level-item"><strong>All</strong></p>
-          <p class="level-item"><a>Published</a></p>
-          <p class="level-item"><a>Drafts</a></p>
-          <p class="level-item"><a>Deleted</a></p>
           <p class="level-item"><a class="button is-success">New</a></p>
         </div>
       </nav>
