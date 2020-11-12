629f3d4f0c9a252f31a6cc8dc4c6b9610f05f36d
Maciek Krzemianowski <m.krzemianowski@gmail.com>
2020-11-12 15:49:37 +0100

    [HOTFIX] Remove index from attribute name.

--- server/models/PageType.js
+++ server/models/PageType.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const pageTypeAttributeSchema = new Schema({
-  name: { type: String, require: true, unique: true, min: 3, max: 30 },
+  name: { type: String, require: true, min: 3, max: 30 },
   description: { type: String, min: 10, max: 250 },
   type: { type: Schema.Types.ObjectId, require: true, ref: 'PageAttributeType' },
 });
