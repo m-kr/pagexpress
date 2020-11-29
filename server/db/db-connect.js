8dc7c4364e0dbdee4256d7ce50c43f32956426ae
Maciek Krzemianowski <m.krzemianowski@gmail.com>
2020-11-29 19:17:46 +0100

    Setup migration system to mongodb * User role migration

--- server/db/db-connect.js
+++ server/db/db-connect.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dbConnect = ({ host, port, collection, user, password }) => {
   mongoose.connect(`mongodb://${host}:${port}/${collection}`, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     auth: {
       authSource: 'admin',
     },
