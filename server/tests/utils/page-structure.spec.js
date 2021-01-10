91196bcc2d8e28788860a5de5e963ae8fb7ac54b
Maciek Krzemianowski <m.krzemianowski@gmail.com>
2021-01-10 23:49:36 +0100

    Fix deep nested components in page structure * Test page structure builder * Fix wrong version in package.json
    Closes #128

--- server/tests/utils/page-structure.spec.js
+++ server/tests/utils/page-structure.spec.js
@@ -1,7 +1,9 @@
-const pageData = require('../mock/page');
+const pageDetails = require('../mock/page-details.json');
+const componentsPatterns = require('../mock/component-patterns.json');
+
 const {
   prettifyComponentStructure,
-  fillAllNestedLevels,
+  fillNestedLevels,
   findNestedComponents,
   getRootComponents,
   buildPageStructure,
@@ -10,14 +12,14 @@ const {
 describe('page-structure', () => {
   let components = null;
   beforeEach(() => {
-    components = [...pageData.components];
+    components = [...pageDetails.components];
   });
 
   describe('prettifyComponentStructure', () => {
     test('Should flatten component structure', () => {
-      const sampleComponent = components[0];
-      const { name, description } = sampleComponent.componentType;
-      const flatComponent = prettifyComponentStructure(sampleComponent);
+      const sampleComponent = pageDetails.components[0];
+      const { name, description } = componentsPatterns[0];
+      const flatComponent = prettifyComponentStructure(componentsPatterns, sampleComponent);
 
       expect(name).toEqual(flatComponent.name);
       expect(description).toEqual(flatComponent.description);
@@ -27,9 +29,9 @@ describe('page-structure', () => {
 
   describe('getRootComponents', () => {
     test('Should return components without parentComponentId', () => {
-      const existingComponentsWithParent = components.some((component) => !!component.parentComponentId);
+      const existingComponentsWithParent = components.some(component => !!component.parentComponentId);
       const rootComponents = getRootComponents(components);
-      const rootComponentsWithParent = rootComponents.some((component) => !!component.parentComponentId);
+      const rootComponentsWithParent = rootComponents.some(component => !!component.parentComponentId);
 
       expect(existingComponentsWithParent).toBeTruthy();
       expect(rootComponentsWithParent).toBeFalsy();
@@ -37,47 +39,57 @@ describe('page-structure', () => {
   });
 
   describe('findNestedComponents', () => {
-    test('Should find 3 nested components', () => {
-      const targetComponent = components.find((c) => c._id === '2');
+    test('Should find 1 nested component', () => {
+      const targetComponent = components.find(c => c._id === '2');
       const foundNestedComponents = findNestedComponents(targetComponent._id, components);
 
-      expect(foundNestedComponents).toHaveLength(2);
+      expect(foundNestedComponents).toHaveLength(1);
     });
 
     test('Should return empty array', () => {
-      const targetComponent = components.find((c) => c._id === '1');
+      const targetComponent = components[components.length - 1];
       const foundNestedComponents = findNestedComponents(targetComponent._id, components);
 
       expect(foundNestedComponents).toHaveLength(0);
     });
   });
 
-  describe('fillAllNestedLevels', () => {
+  describe('fillNestedLevels', () => {
     test('Should enrich component with first children level', () => {
-      const parentComponent = components.find((c) => c._id === '2');
-      const enrichedComponent = fillAllNestedLevels(parentComponent, components);
+      const parentComponent = components[0];
+      const enrichedComponent = fillNestedLevels([...components], { ...parentComponent });
 
       expect(enrichedComponent.components).toBeDefined();
-      expect(enrichedComponent.components).toHaveLength(2);
+      expect(enrichedComponent.components).toHaveLength(1);
     });
 
     test('Should enrich component with second children level', () => {
-      const parentComponent = components.find((c) => c._id === '2');
-      const enrichedComponent = fillAllNestedLevels(parentComponent, components);
-      const nestedComponentWithChild = enrichedComponent.components.find((c) => c._id === '4');
+      const parentComponent = components[0];
+      const enrichedComponent = fillNestedLevels(components, parentComponent);
+      const nestedComponentWithChild = enrichedComponent.components[0];
 
-      expect(nestedComponentWithChild.components).toBeDefined();
+      expect(enrichedComponent.components).toHaveLength(1);
       expect(nestedComponentWithChild.components).toHaveLength(1);
+    });
+
+    test('Should enrich component with third children level', () => {
+      const parentComponent = components[0];
+      const enrichedComponent = fillNestedLevels(components, parentComponent);
+      const firstNestedComponentWithChild = enrichedComponent.components[0];
+      const secondNestedComponentWithChild = firstNestedComponentWithChild.components[0];
+
+      expect(enrichedComponent.components).toHaveLength(1);
+      expect(firstNestedComponentWithChild.components).toHaveLength(1);
+      expect(secondNestedComponentWithChild.components).toHaveLength(1);
     });
   });
 
   describe('buildPageStructure', () => {
     test('Should create nested structure from components list', () => {
-      expect(components).toHaveLength(5);
-      const structure = buildPageStructure(components);
-      expect(structure).toHaveLength(2);
-      expect(structure[1].components).toBeDefined();
-      expect(structure[1].components).toHaveLength(2);
+      expect(components).toHaveLength(4);
+      const structure = buildPageStructure(components, componentsPatterns);
+      expect(structure).toHaveLength(1);
+      expect(structure[0].components).toHaveLength(1);
     });
   });
 });
